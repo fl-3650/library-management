3dbcaa6a2e6886219b90d47046c811ef9a8571d8
flynt <repilovi@gmail.com>
2024-08-06 19:02:31 +0300

    i cant hold it in anymore, i just wanna tudududu

--- src/main/java/ru/repilov/library/dao/BookDAO.java
+++ src/main/java/ru/repilov/library/dao/BookDAO.java
@@ -6,17 +6,21 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.repilov.library.models.Book;
+import ru.repilov.library.models.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
 
     private final JdbcTemplate jdbcTemplate;
+    private final PersonDAO personDAO;
 
     @Autowired
-    public BookDAO(JdbcTemplate jdbcTemplate) {
+    public BookDAO(JdbcTemplate jdbcTemplate, PersonDAO personDAO) {
         this.jdbcTemplate = jdbcTemplate;
+        this.personDAO = personDAO;
     }
 
     public List<Book> index() {
@@ -29,16 +33,30 @@ public class BookDAO {
     }
 
     public void save(@NotNull Book book) {
-        jdbcTemplate.update("INSERT INTO Book(title, author, year) VALUES(?, ?, ?)", book.getTitle(),
-                book.getAuthor(), book.getYear());
+        jdbcTemplate.update("INSERT INTO Book(title, author, year) VALUES(?, ?, ?)",
+                book.getTitle(), book.getAuthor(), book.getYear());
     }
 
     public void update(int id, @NotNull Book updatedBook) {
-        jdbcTemplate.update("UPDATE Book SET title=?, author=?, year=? WHERE id=?", updatedBook.getTitle(),
-                updatedBook.getAuthor(), updatedBook.getYear(), id);
+        jdbcTemplate.update("UPDATE Book SET title=?, author=?, year=? WHERE id=?",
+                updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getYear(), id);
     }
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Book WHERE id=?", id);
+    }
+
+    public void assign(int id, @NotNull Person selectedPerson) {
+        jdbcTemplate.update("UPDATE book SET person_id=? WHERE id=?",
+                selectedPerson.getId(), id);
+    }
+
+    public void release(int id) {
+        jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE id=?", id);
+    }
+
+    public Optional<Person> getBookOwner(int id) {
+        return jdbcTemplate.query("SELECT person.* FROM person JOIN book ON person.id = book.person_id WHERE" +
+                " book.id=?", new BeanPropertyRowMapper<>(Person.class), id).stream().findAny();
     }
 }
